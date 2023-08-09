4c493c0bf7de9c7aafe9c500bdb4cbc37fd39655
Vahid Ghasemi <88976506+vahid97gh@users.noreply.github.com>
2023-08-09 21:15:38 +0330

    Create zabbix-agent.sh

---
+++ zabbix-agent.sh
@@ -0,0 +1,29 @@
+#!/bin/bash/
+Resolv= /etc/resolv.conf
+if [ -f "$Resolv"]; then
+echo "resolv is exist!"
+else
+sudo touch /etc/resolv.conf
+echo "nameserver 8.8.8.8 \nnameserver 4.2.2.4" > /etc/resolv.conf
+fi
+apt update -y
+File=/home/ubuntu/zabbix-release_5.0-1+focal_all.deb
+if [ -f "$File"]; then
+echo "file does exist!"
+else
+echo "file does not exist!"
+wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+focal_all.deb
+fi
+sudo dpkg -i zabbix-release_5.0-1+focal_all.deb
+sudo apt update
+sudo apt install zabbix-agent -y
+service_name="zabbix-agent"
+if systemctl is-active --quiet "$service_name.service" ; then
+echo "$service_name running"
+else
+systemctl start "$service_name"
+fi
+sed -i 's/LogFileSize=0/LogFileSize=1/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/Hostname=Zabbix server/Hostname=zabbix-agent/' /etc/zabbix/zabbix_agentd.conf
+sudo systemctl restart zabbix-agent
+sudo systemctl enable zabbix-agent
